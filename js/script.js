2188f1df4d80d7a9d396d8754f21b78def2c94e1
Parham <155875539+parham2389@users.noreply.github.com>
2025-01-17 21:42:37 -1200

    commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+﻿let $ = document
+// Variabels
+let body = _class("body")
+let contextMenu = _class("context-menu")
+// Functions 
+function _tagName(tagName) {
+    return $.querySelector(tagName)
+}
+function _class(className) {
+    return $.querySelector("." + className)
+}
+function keyBordHandler() {
+    contextMenu.setAttribute("style", "display: none;")
+}
+function contextMenuHandler() {
+    event.preventDefault()
+    contextMenu.setAttribute("style", "display: block; left: " + event.pageX + "px; top: " + event.pageY + "px;")   
+}
+// Site Codes
+body.addEventListener("click", keyBordHandler)
+body.addEventListener("keydown", keyBordHandler)
+body.addEventListener("contextmenu", contextMenuHandler)
